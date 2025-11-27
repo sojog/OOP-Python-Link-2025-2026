9c9feec9df6c9a7b20d972290fc66962c4bbe109
Silviu Ojog <sil.ojog@gmail.com>
2025-11-27 18:15:43 +0200

    Intro OOP

---
+++ 01.intro.py
@@ -0,0 +1,28 @@
+## OOP. -> Object Oriented Programming
+## POO -> Programare Orientata Obiect
+
+variabila = 20 # variabila
+print(variabila)
+
+## declaratie -> cu litera mica
+def functie(): # functie
+    x = 10
+    print(x)
+
+functie()
+functie()
+functie()
+functie()
+functie()
+
+## declaratie -> cu litera mare
+class Clasa:
+    # atribut
+    atribut = 20
+    
+    ## metoda
+    def metoda(self):
+        x = 10
+        print(x)
+
+    
